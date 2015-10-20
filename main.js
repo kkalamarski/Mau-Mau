5ea1e170c87bbb782b6aa88be577cd3c24f1fc2c
Krzysztof Kalamarski <kkalamarski@gmail.com>
2015-10-20 21:39:07 +0300

    Remove redundant libs

--- main.js
+++ main.js
@@ -1,18 +1,5 @@
 /* Loading Libs */
-var angular = require('angular');
 jQuery = $ = require('jquery');
 var bootstrap = require('bootstrap');
-require('angular-ui-router');
 
-/* Loading Modules */
-require('./src/js/Application');
 
-/* Init App */
-var app = angular.module('app', [
-    'ui.router',
-    'Application'
-]);
-
-app.config(function($urlRouterProvider){
-    $urlRouterProvider.otherwise('home');
-});
